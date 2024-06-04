d297eeff9c32c434c0d21d25416bcf129b69f100
mdeport <mateo.deport@ecole-isitech.fr>
2024-06-04 16:08:30 +0200

    begin egain

---
+++ components/supabaseClient.js
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+import env from "../.env.js";
+
+const supabaseUrl = env.SUPABASE_URL;
+const supabaseAnonKey = env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  });
--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import env from "../.env.js";
-
-const supabaseUrl = env.SUPABASE_URL;
-const supabaseAnonKey = env.SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
