3e6da35e7cc55f0439d9f168c1be6a7ce1396057
mdeport <mateo.deport@ecole-isitech.fr>
2024-06-05 16:45:42 +0200

    add icon profil for page account

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,14 @@
 import { Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index"   options={{ title: "Pizza", tabBarIcon: ({ color }) => <Ionicons name="pizza-sharp" size={28} color={color} /> }} />
-      <Tabs.Screen name="Account" options={{ title: "Account", tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} /> }} />
+      <Tabs.Screen name="index" options={{ title: "Pizza", tabBarIcon: ({ color }) => <Ionicons name="pizza-sharp" size={28} color={color} /> }} />
       <Tabs.Screen name="CreatePizza" options={{ title: "create pizza", tabBarIcon: ({ color }) => <Ionicons size={28} name="add" color={color} /> }} />
+      <Tabs.Screen name="Account" options={{ title: "Account", tabBarIcon: ({ color }) => <MaterialIcons name="account-circle" size={24} color={color} /> }} />
     </Tabs>
   );
 }
