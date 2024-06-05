a48cd5a017f4469e35665c7b6ee935bcf63bbb7c
mdeport <mateo.deport@ecole-isitech.fr>
2024-06-05 15:14:40 +0200

    add css in page updatepizza

--- app/(tabs)/UpdatePizza.tsx
+++ app/(tabs)/UpdatePizza.tsx
@@ -1,10 +1,13 @@
-import { ScrollView, Text, View, Image, TextInput, StyleSheet } from "react-native";
+import { ScrollView, Text, View, Image, TextInput, StyleSheet, Button } from "react-native";
 import { supabase } from "../../lib/supabase";
 import { useState, useEffect } from "react";
 
 export default function Editpizza(/*idpizza: string*/) {
   const idpizza = 1;
   const [pizza, setPizza] = useState([]);
+  const [namePizza, setNamePizza] = useState("");
+  const [pricePizza, setPricePizza] = useState("");
+  const [ingredientsPizza, setIngredientsPizza] = useState("");
 
   useEffect(() => {
     fetchpizzabyid();
@@ -17,36 +20,45 @@ export default function Editpizza(/*idpizza: string*/) {
       console.error("Error fetching data:", error);
       return;
     }
+    if (data && data.length > 0) {
+      setNamePizza(data[0].pizza);
+      setPricePizza(data[0].price.toString());
+      setIngredientsPizza(data[0].ingredients.join(", "));
+    }
     setPizza(data);
   }
 
-  const NamePizza = "";
-  const PricePizza = 0;
-  const IngredientsPizza = [];
-
   async function updatepizza() {
-    const { data, error } = await supabase.from("pizzas").update({ pizza: NamePizza, price: PricePizza, ingredients: IngredientsPizza }).eq("id", idpizza);
+    const { data, error } = await supabase
+      .from("pizzas")
+      .update({ pizza: namePizza, price: pricePizza, ingredients: ingredientsPizza.split(", ") })
+      .eq("id", idpizza);
 
     if (error) {
-      console.error("Error updating data:", error);
+      console.error("Error updating pizza:", error);
+      alert("Erreur lors de la mise à jour de la pizza, veuillez réessayer ultérieurement.");
       return;
     }
-    console.log("Data updated successfully", data);
+    alert("Pizza mise à jour avec succès");
+    fetchpizzabyid();
   }
 
   return (
     <ScrollView style={styles.container}>
       {pizza ? (
-        <View style={styles.content}>
-          <Text style={styles.title}>Editer la Pizza</Text>
-          {pizza.map((pizza) => (
-            <View key={pizza.id} style={styles.pizzaContainer}>
-              <TextInput style={styles.input} placeholder="Nom de la pizza" value={pizza.pizza} />
-              <TextInput style={styles.input} placeholder="prix" value={pizza.price.toString()} keyboardType="numeric" />
-              <TextInput style={styles.input} placeholder="description" value={pizza.ingredients.join(", ")} />
-            </View>
-          ))}
-        </View>
+        <>
+          <View style={styles.content}>
+            <Text style={styles.title}>Editer la Pizza</Text>
+            {pizza.map((pizza) => (
+              <View key={pizza.id} style={styles.pizzaContainer}>
+                <TextInput style={styles.input} placeholder="Nom de la pizza" value={namePizza} onChangeText={setNamePizza} />
+                <TextInput style={styles.input} placeholder="prix" value={pricePizza} keyboardType="numeric" onChangeText={setPricePizza} />
+                <TextInput style={styles.input} placeholder="ingredient" value={ingredientsPizza} onChangeText={setIngredientsPizza} />
+              </View>
+            ))}
+          </View>
+          <Button title="Mettre à jour" onPress={updatepizza} />
+        </>
       ) : (
         <Text>Loading...</Text>
       )}
@@ -66,7 +78,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   title: {
-    fontSize: 24,
+    fontSize: 35,
     fontWeight: "bold",
     marginBottom: 20,
   },
@@ -80,5 +92,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     paddingLeft: 10,
     borderRadius: 5,
+    marginBottom: 10,
+    fontSize: 18,
   },
 });
