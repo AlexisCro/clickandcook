ddc72d9c175cb02cdd22e36eefa59f83d47b9ca5
mdeport <mateo.deport@ecole-isitech.fr>
2024-06-05 16:39:11 +0200

    update background container

--- assets/stylesheet/style.js
+++ assets/stylesheet/style.js
@@ -1,48 +1,47 @@
 import { StyleSheet } from "react-native";
 
-const styles= StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor: '#f9e1b1',
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     margin: 0,
   },
   formContainer: {
     borderRadius: 15,
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
     width: 300,
-    zIndex: 100
+    zIndex: 100,
   },
   input: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 10,
     width: 300,
     marginTop: 10,
     marginBottom: 10,
-    padding: 15
+    padding: 15,
   },
-  buttonContainer:{
+  buttonContainer: {
     width: 300,
     padding: 0,
-    margin: 0
+    margin: 0,
   },
   button: {
     borderRadius: 20,
-    width: '100%',
+    width: "100%",
     marginVertical: 10,
   },
   profileContainer: {
-    width: '80%'
+    width: "80%",
   },
   avatarContainer: {
-    marginHorizontal: '20%',
+    marginHorizontal: "20%",
     marginBottom: 20,
-  }
+  },
 });
 
 export default styles;
