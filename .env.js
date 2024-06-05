21dbf84eff2e1acc3a9e9ac8866911e5c3828209
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2024-06-05 09:26:12 +0200

    remove env from github

--- .env.js
+++
@@ -1,6 +0,0 @@
-const env = {
-  "SUPABASE_URL": "https://nwbwiotxnccatejwhssk.supabase.co",
-  "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53Yndpb3R4bmNjYXRlandoc3NrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0OTYxODIsImV4cCI6MjAzMzA3MjE4Mn0.mM4NCqa2dnSjWpLk95sz2Eokn9FCEwzB1vtGfe0YBZg"
-}
-
-export default env;
