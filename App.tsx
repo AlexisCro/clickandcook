0479620fa10aa7c2c8d9541fd44ea508d591f977
mdeport <mateo.deport@ecole-isitech.fr>
2024-06-05 09:22:55 +0200

    modifiy app.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,23 @@
-import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
-import Auth from './components/Auth'
-import Account from './app/(tabs)/Account'
-import { View } from 'react-native'
-import { Session } from '@supabase/supabase-js'
-import styles from './assets/stylesheet/style'
+import { useState, useEffect } from "react";
+import { supabase } from "./lib/supabase";
+import Auth from "./components/Auth";
+import Account from "./app/(tabs)/Account";
+import { View } from "react-native";
+import { Session } from "@supabase/supabase-js";
+import styles from "./assets/stylesheet/style";
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
-  return (
-    <View style={styles.container}>
-      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
-    </View>
-  )
+  return <View style={styles.container}>{session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}</View>;
 }
---
+++ app/(tabs)/commande.tsx
@@ -0,0 +1,11 @@
+import { ScrollView, Text, View, Image } from "react-native";
+
+export default function Commande() {
+  return (
+    <ScrollView>
+      <View>
+        <Text>Commande</Text>
+      </View>
+    </ScrollView>
+  );
+}
