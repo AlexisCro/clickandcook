552a3bf9ab805818aeb070948084e9633910bcd9
mdeport <mateo.deport@ecole-isitech.fr>
2024-06-06 15:37:43 +0200

    add fichier zip in aab for deployement

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-import env from "../.env.mjs";
 
-const supabaseUrl = env.SUPABASE_URL;
-const supabaseAnonKey = env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
